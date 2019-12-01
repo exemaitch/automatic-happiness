1ae48acaadd4c20bec94d9f5e90804f0fa732c3c
Xuan Mai Ho <xuan.mai.ho@jetty.com>
2019-12-01 15:34:17 -0500

    Mistake in defining the context

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class S3Bucket(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, *kwargs)
-        bucket = aws_s3.Bucket(scope, "MyTestBucket")
+        bucket = aws_s3.Bucket(self, "MyTestBucket")
 
 app = core.App()
 S3Bucket(app, "S3BucketTest")
